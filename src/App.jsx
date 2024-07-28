0d1d43522bb247b0f9d7e3a2bdb77e5da8c1e0cd
Granshmeyr <playstationmelvin@gmail.com>
2024-07-28 19:09:15 -0400

    ui prototype finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,63 +4,101 @@ import { invoke } from "@tauri-apps/api/core";
 import { React, useState, cloneElement } from "react";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
-import { darken } from "@mui/material";
-
+import {
+  Button,
+  Checkbox,
+  darken,
+  FormControlLabel,
+  FormGroup,
+} from "@mui/material";
 import "./App.css";
-import { Box, Divider, ListItemText, Paper, Typography } from "@mui/material";
+import {
+  Box,
+  Stack,
+  Divider,
+  ListItemText,
+  Paper,
+  Typography,
+  useTheme,
+} from "@mui/material";
 
-function generateFileList(strings) {
+function App() {
+  const theme = useTheme();
+  const scriptLabels = ["Copy objects", "Copy groups", "Copy layers"];
+
   return (
-    <List dense>
-      {strings.map((v, i) =>
-        cloneElement(
-          <ListItem
-            sx={(theme) => ({
-              padding: theme.spacing(0, 1.5),
-            })}
+    <Box sx={{ padding: theme.spacing(3), flexGrow: 1 }}>
+      <Grid container spacing={theme.spacing(2)}>
+        <Grid item xs={12}>
+          <Paper sx={{ padding: theme.spacing(2) }}>
+            <LayoutSelector header={"From layout:"} />
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper sx={{ padding: theme.spacing(2) }}>
+            <LayoutSelector header={"To layouts:"} />
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              width: "100%",
+              gap: theme.spacing(2),
+            }}
           >
-            <ListItemText primary={v} />
-          </ListItem>,
-          { key: i }
-        )
-      )}
-    </List>
+            <Box sx={{ width: "30%" }}>
+              <Button sx={{ width: "100%" }} variant="contained">
+                Apply changes
+              </Button>
+            </Box>
+            <Paper sx={{ width: "70%", padding: theme.spacing(2) }}>
+              <FormGroup>
+                {scriptLabels.map((label) => (
+                  <FormControlLabel
+                    key={label}
+                    control={<Checkbox />}
+                    label={label}
+                  ></FormControlLabel>
+                ))}
+              </FormGroup>
+            </Paper>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
-function App() {
+function LayoutSelector({ header, layouts }) {
+  const theme = useTheme();
+  const hdr = header ? header : "Layout:";
+  const lyts = layouts ? layouts : Array(20).fill("layout.json");
+
   return (
-    <Box
-      sx={(theme) => ({
-        padding: theme.spacing(3),
-        flexGrow: 1,
-      })}
-    >
-      <Paper>
-        <Grid sx={(theme) => ({ padding: theme.spacing(2) })}>
-          <Grid item xs={12}>
-            <Box>
-              <Typography
-                sx={(theme) => ({ paddingBottom: theme.spacing(2) })}
-                variant="h6"
-              >
-                From folder:
-              </Typography>
-              <Divider />
-              <Paper
-                sx={(theme) => ({
-                  backgroundColor: darken(
-                    theme.palette.background.default,
-                    0.02
-                  ),
-                  boxShadow: "inset 0 2px 4px 0 rgba(0,0,0,0.2)",
-                })}
-              >
-                {generateFileList(Array(6).fill("file.txt"))}
-              </Paper>
-            </Box>
-          </Grid>
-        </Grid>
+    <Box>
+      <Typography sx={{ paddingBottom: theme.spacing(2) }} variant="h6">
+        {hdr}
+      </Typography>
+      <Paper
+        sx={{
+          backgroundColor: darken(theme.palette.background.default, 0.02),
+          boxShadow: "inset 0 2px 4px 0 rgba(0,0,0,0.2)",
+        }}
+      >
+        <Box sx={{ maxHeight: 180, overflow: "auto" }}>
+          <List dense>
+            {lyts.map((v, i) =>
+              cloneElement(
+                <ListItem sx={{ padding: theme.spacing(0, 1.5) }}>
+                  <ListItemText primary={v} />
+                </ListItem>,
+                { key: i }
+              )
+            )}
+          </List>
+        </Box>
       </Paper>
     </Box>
   );
